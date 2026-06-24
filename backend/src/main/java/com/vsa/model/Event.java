058f8a0841995d7ba4e7930d39bc20dd5e868624
Tuan Ho <hhatuan2008@gmail.com>
2026-06-24 01:38:43 -0700

    add vsaGen

--- backend/src/main/java/com/vsa/model/Event.java
+++ backend/src/main/java/com/vsa/model/Event.java
@@ -2,19 +2,23 @@ package com.vsa.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@RequiredArgsConstructor
 public class Event {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String category;
     private String date;
+    private String vsaGen;
     private boolean ongoing;
 }
