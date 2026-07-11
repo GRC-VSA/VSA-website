5957d89462562bf1941664741fe431010c8e4405
Tuan Ho <hhatuan2008@gmail.com>
2026-07-10 20:54:39 -0700

    Feat added UserRepository and UserController init

---
+++ backend/src/main/java/com/vsa/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.vsa.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:5173")
+public class UserController {
+
+}
---
+++ backend/src/main/java/com/vsa/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.vsa.repository;
+
+import com.vsa.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationToken(String token);
+    Optional<User> findByResetToken(String token);
+    boolean existsByEmail(String email);
+}
