c47407d1c86cf85450b8de86b7770593d431ca76
Tuan Ho <hhatuan2008@gmail.com>
2026-07-12 00:54:20 -0700

    revert back to full name to test

--- backend/src/main/java/com/vsa/model/User.java
+++ backend/src/main/java/com/vsa/model/User.java
@@ -16,11 +16,9 @@ public class User {
   @Column(name = "SID")
   private String sid;
 
-  @Column(name = "first_name", nullable = false)
-  private String firstName;
-
-  @Column(name = "last_name", nullable = false)
-  private String lastName;
+  //TODO: Split to first and last name once table ready
+  @Column(name = "full_name", nullable = false)
+  private String fullName;
 
   @Column(nullable = false, unique = true)
   private String email;
