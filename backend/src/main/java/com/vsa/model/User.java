4322071afee147a8f004e2bbff830096ca12dee4
Tuan Ho <hhatuan2008@gmail.com>
2026-07-12 01:01:58 -0700

    change back to split first and last name

--- backend/src/main/java/com/vsa/model/User.java
+++ backend/src/main/java/com/vsa/model/User.java
@@ -16,9 +16,11 @@ public class User {
   @Column(name = "SID")
   private String sid;
 
-  //TODO: Split to first and last name once table ready
-  @Column(name = "full_name", nullable = false)
-  private String fullName;
+  @Column(name = "first_name", nullable = false)
+  private String firstName;
+
+  @Column(name = "last_name", nullable = false)
+  private String lastName;
 
   @Column(nullable = false, unique = true)
   private String email;
