5b2bdcff83223a9c079dac33f663672df102afe7
Tuan-Ho <hhatuan2008@gmail.com>
2026-08-22 21:04:37 -0700

    update controller for registration

--- backend/src/main/java/com/vsa/controller/QuestionTypeController.java
+++ backend/src/main/java/com/vsa/controller/QuestionTypeController.java
@@ -1,4 +1,25 @@
 package com.vsa.controller;
 
+import com.vsa.model.QuestionType;
+import com.vsa.service.QuestionTypeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/question-types")
 public class QuestionTypeController {
+    private final QuestionTypeService questionTypeService;
+
+    public QuestionTypeController(QuestionTypeService questionTypeService) {
+        this.questionTypeService = questionTypeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<QuestionType>> getQuestionTypes(){
+        return ResponseEntity.ok(questionTypeService.getAllQuestionTypes());
+    }
 }
